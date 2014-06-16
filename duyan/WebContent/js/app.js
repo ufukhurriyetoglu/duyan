cf5cd69536f0ec70c879a97eb7bdb5b5924ce271
einan <emrahinan@gmail.com>
2014-06-16 18:25:25 +0300

    angularjs file is created.

--- duyan/WebContent/js/app.js
+++ duyan/WebContent/js/app.js
@@ -10,7 +10,7 @@
 								var url = "http://localhost:8080/duyan/DuyanServlet?content=Yap%C4%B1lan+d%C3%BC%C4%9F%C3%BCnde+Arda+Turan+ve+Hakan+%C5%9E%C3%BCk%C3%BCr+haz%C4%B1r+bulundu.&outputtype=Json";
 								var store = this;
 								store.products = [];
-								$http.get(url).success(
+								$http.get('mybr.json').success(
 										function(data) {
 											store.products = data;
 											console.log("json : "
