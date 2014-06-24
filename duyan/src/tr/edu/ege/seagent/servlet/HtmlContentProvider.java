1b983118e793bf8d02130ba43d947b7438dad10d
einan <emrahinan@gmail.com>
2014-06-24 19:24:36 +0300

    entity colors are defined.

--- duyan/src/tr/edu/ege/seagent/servlet/HtmlContentProvider.java
+++ duyan/src/tr/edu/ege/seagent/servlet/HtmlContentProvider.java
@@ -183,7 +183,7 @@ public class HtmlContentProvider {
 		if (type.equals("Person"))
 //			color = "#9CC2E6";
 			color = "btn btn-warning btn-xs active";
-		else if (type.equals("Organization"))
+		else if (type.equals("Organisation"))
 //			color = "#C1E19E";
 			color = "btn btn-default btn-xs active";
 		else
